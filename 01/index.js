306dc0ce5e3afa35d777fe07c5c655929e91a27b
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-23 16:30:11 -0300

    programa que cria um array de números + sua soma total

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,24 @@
 const numeros = [2, 3, 4, 6];
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    soma = soma + numeros[i];
+}
+
+console.log(soma);
+
+// Imprime "15"
+
+/* Exemplos
+const numeros = [1, 5, 8, 9];
+Imprime "23"
+
+const numeros = [5, 12, 23, 18, 49, 63];
+Imprime "170" 
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+Imprime "45"
+
+const numeros = [2, 3];
+Imprime "5"
+*/
