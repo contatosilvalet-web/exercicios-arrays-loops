65ac50b41ad3fd6857e2eeaa86a034ae97352457
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-24 13:41:31 -0300

    contador de letras com o exemplo na letra A

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,18 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = ["A", "a", "B", "C", "L", "z"];
+let contador = 0;
+
+for (let i = 0; i < letras.length; i++) {
+    if (letras[i] === "E" || letras[i] === "e") {
+        contador++; 
+    }
+}
+
+if (contador === 0) {
+    console.log('Nenhuma letra "E" ou "e" foi encontrada.');
+} else if (contador === 1) {
+    console.log('Foi encontrada 1 letra "E" ou "e".');
+} else {
+    console.log(`Foram encontradas ${contador} letras "E" ou "e".`);
+}
+
+// Imprime "Nenhuma letra "E" ou "e" foi encontrada."
