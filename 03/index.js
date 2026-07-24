76d880cf4b76afcdea75453955b0d51cdcdd941c
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-24 15:14:32 -0300

    resolvendo o exercicio da posição do numero 10 ou se nao tiver numero dez, aparecer -1

--- 03/index.js
+++ 03/index.js
@@ -1,2 +1,40 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+// LETRA A
+
+const numeros = [54, 22, 14, 87, 284];
+let posicaoDoDez = -1;
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] === 10) {
+        posicaoDoDez = i; 
+        break; 
+    }
+}
+
+console.log(posicaoDoDez);
+
+// Imprime -1.
+
+// LETRA B
+const numeros = [54, 22, 14, 10, 284];
+let posicaoDoDez = -1;
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] === 10) {
+        posicaoDoDez = i; 
+        break;
+    }
+}
+
+console.log(posicaoDoDez);
+
+// Imprime 3.
+
+/* Outros exemplos
+const numeros = [10, 22, 14, 87, 284];
+Imprime 0.
+
+const numeros = [54, 22, 14, 87, 10];
+Imprime 4.
+*/
+
 
