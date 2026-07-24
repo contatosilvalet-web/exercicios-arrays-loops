759f4e9bfa780d37e7ec34ecbc151b5f52f5c000
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-24 15:32:56 -0300

    programa para calcular a soma dos números pares presentes no array

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,25 @@
 const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+let soma = 0;
 
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+        soma += numeros[i]; 
+    }
+}
+
+console.log(soma);
+
+// Imprime "28"
+
+/* Outros exemplos
+const numeros = [12, 16, 22, 25, 30, 31, 45, 48];
+Imprime 128.
+
+const numeros = [2,4,6];
+Imprime 12.
+
+const numeros = [15,27,35,41];
+Imprime 0.
+
+const numeros = [13, 25, 30, 33, 45];
+Imprime 30. */
