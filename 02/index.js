4bfa59b70c10a0172e30b9a3fbd686e0c65424c5
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-24 13:42:10 -0300

    contador de letras com o exemplo da LETRA A e da LETRA B do exercicio, associado a outros exemplos

--- 02/index.js
+++ 02/index.js
@@ -1,3 +1,5 @@
+//LETRA A
+
 const letras = ["A", "a", "B", "C", "L", "z"];
 let contador = 0;
 
@@ -16,3 +18,32 @@ if (contador === 0) {
 }
 
 // Imprime "Nenhuma letra "E" ou "e" foi encontrada."
+
+// LETRA B
+const letras = ["A", "e", "B", "C", "E", "z"];
+let contador = 0;
+
+for (let i = 0; i < letras.length; i++) {
+
+    if (letras[i] === "E" || letras[i] === "e") {
+        contador++; 
+    }
+}
+
+if (contador === 0) {
+    console.log('Nenhuma letra "E" ou "e" foi encontrada.');
+} else if (contador === 1) {
+    console.log('Foi encontrada 1 letra "E" ou "e".');
+} else {
+    console.log(`Foram encontradas ${contador} letras "E" ou "e".`);
+}
+
+// Imprime "Foram encontradas 2 letras "E" ou "e"."
+
+/* Outros exemplos
+const letras = ["A", "e", "B", "C", "E", "z", "e"];
+Imprime "Foram encontradas 3 letras "E" ou "e"."
+
+const letras = ["A", "e", "B", "C", "E", "z", "e", "E"];
+Imprime "Foram encontradas 4 letras "E" ou "e"."
+*/
