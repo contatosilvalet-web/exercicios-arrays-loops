147fc87fc954a8db162fb2a3ef4128abcea69c4f
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-25 09:33:19 -0300

    programa para filtrar numeros estejam entre 10 e 20 e maiores que 100
    Updated the filtering logic to include additional test cases and examples.

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,42 @@
+// LETRA A
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
+let filtrados = [];
+
+
+for (let i = 0; i < original.length; i++) {
+    let numero = original[i];
+    if ((numero >= 10 && numero <= 20) || numero > 100) {
+        filtrados.push(numero);
+    }
+}
+
+console.log(filtrados);
+
+// Imprime [ 13, 17, 118, 245 ].
+
+// LETRA B
+const original = [5, 7, 10, 13, 17, 21, 26, 34, 100, 118, 245];
+let filtrados = [];
+
+
+for (let i = 0; i < original.length; i++) {
+    let numero = original[i];
+    if ((numero >= 10 && numero <= 20) || numero > 100) {
+        filtrados.push(numero);
+    }
+}
+
+console.log(filtrados);
+
+// Imprime [ 10, 13, 17, 118, 245 ].
+
+/* Outros exemplos
+const original = [6, 19, 66, 99];
+Imprime [ 19 ]
+
+const original = [12, 14, 19, 23, 26, 27, 30, 45, 60];
+Imprime [ 12, 14, 19 ]. 
+
+const original = [12, 14, 16, 113 ];
+Imprime [ 12, 14, 16, 113 ] */ 
 
