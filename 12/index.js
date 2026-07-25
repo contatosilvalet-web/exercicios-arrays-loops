0643e2b39c0c365a00fcfcf0de341a7374e797ce
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-25 09:42:42 -0300

    programa array para controlar fila de um banco com pessoas dentro e fora

--- 12/index.js
+++ 12/index.js
@@ -1,3 +1,45 @@
+// EXEMPLO 01
 const filaDeDentro = ["Jose", "Maria", "Joao"];
 const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+
+while (filaDeDentro.length < 5 && filaDeFora.length > 0) {
+    const proximaPessoa = filaDeFora.shift();
+    filaDeDentro.push(proximaPessoa);
+}
+
+console.log(filaDeDentro);
+console.log(filaDeFora);
+
+/* Imprime [ 'Jose', 'Maria', 'Joao', 'Joana', 'Roberta' ]
+[ 'Marcos', 'Felipe' ] */
+
+// EXEMPLO 02
+
+const filaDeDentro = ["Jose", "Joao"];
+const filaDeFora = ["Joana", "Roberta"];
+
+while (filaDeDentro.length < 5 && filaDeFora.length > 0) {
+    const proximaPessoa = filaDeFora.shift();
+    filaDeDentro.push(proximaPessoa);
+}
+
+console.log(filaDeDentro);
+console.log(filaDeFora);
+
+/* Imprime [ 'Jose', 'Joao', 'Joana', 'Roberta' ]
+[] */ 
+
+/* Outros exemplos
+const filaDeDentro = ["Jose", "Joao", "Carla", "Vitor", "Lucia"];
+const filaDeFora = ["Joana", "Roberta", "Pedro", "Paulo", "Pietro"];
+
+Imprime [ 'Jose', 'Joao', 'Carla', 'Vitor', 'Lucia' ]
+[ 'Joana', 'Roberta', 'Pedro', 'Paulo', 'Pietro' ] 
+
+const filaDeDentro = ["Vitor", "Lucia"];
+const filaDeFora = [];
+
+Imprime [ 'Vitor', 'Lucia' ]
+[]
+*/ 
 
