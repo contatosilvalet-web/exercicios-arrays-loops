4755c284df1c272f2869aee19415133489e86318
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-25 09:57:26 -0300

    programa para imprimir o menor valor encontrado entre os dois arrays
    Added examples comparing elements of two arrays and printing the smaller value.

--- 14/index.js
+++ 14/index.js
@@ -1,3 +1,58 @@
+// Exemplo 01
 const arrayA = [5, 32, 3, 44, 1];
 const arrayB = [57, 4, 21, 2, 13];
 
+for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] < arrayB[i]) {
+        console.log(arrayA[i]);
+    } else {
+        console.log(arrayB[i]);
+    }
+}
+
+/* Imprime
+5
+4
+3
+2
+1 */
+
+// Exemplo 02 
+const arrayA = [1, 3, 3, 44, 11];
+const arrayB = [57, 4, 21, 32, 13];
+
+for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] < arrayB[i]) {
+        console.log(arrayA[i]);
+    } else {
+        console.log(arrayB[i]);
+    }
+}
+
+/* Imprime
+1
+3
+3
+32
+11 */
+
+/* Outros exemplos
+const arrayA = [2,12,45,68,73];
+const arrayB = [69,79,85,89,99];
+
+Imprime 
+2
+12
+45
+68
+73
+
+const arrayA = [6,13,18,19,23];
+const arrayB = [30,36,45,57,61];
+Imprime
+6
+13
+18
+19
+23
+*/
