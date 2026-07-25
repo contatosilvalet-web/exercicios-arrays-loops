685420060399cff30c583465bbfd1c86140257b8
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-25 09:52:10 -0300

    programa array + truthiness para verificar em uma caixa de disjuntores quais estão ligados

--- 13/index.js
+++ 13/index.js
@@ -1,2 +1,40 @@
+// Exemplo 01
 const disjuntores = [false, false, true, false, false, true, false, false];
 
+for (let i = 0; i < disjuntores.length; i++) {
+    if (disjuntores[i]) {
+        console.log(i);
+    }
+}
+
+/* Imprime 
+2
+5 */
+
+// Exemplo 02
+const disjuntores = [
+  false,
+  true,
+  true,
+  false,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+for (let i = 0; i < disjuntores.length; i++) {
+    if (disjuntores[i]) {
+        console.log(i);
+    }
+}
+
+/* Imprime 
+1
+2
+5
+8
+9
+*/ 
